2c85b49b51c18bbcc984fc9064d14433c971d5af
jmdmpolo <jmdmpolo@users.noreply.github.com>
2018-01-12 10:04:19 +0100

    add comments

--- mygeth_local.sh
+++ mygeth_local.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# runs local geth node, running private blockchain
+# blockchain data must be initialized in $HOME/.ethereum/private_network folder
+# uses 1234 as networkid
+
 geth --identity "localNode" --rpc --rpcaddr "0.0.0.0" --rpcport "8545" --rpccorsdomain "*" --datadir "$HOME/.ethereum/private_network" --port "30303" --nodiscover --rpcapi "db,eth,net,personal,web3" --networkid 1234
+
