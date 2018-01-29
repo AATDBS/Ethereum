799f4154facc74360f25adfb3adc96ed40cb1aeb
jmdmpolo <jmdmpolo@users.noreply.github.com>
2018-01-29 12:50:07 +0100

    Update SimpleEtherProof.js

--- SimpleEtherProof.js
+++ SimpleEtherProof.js
@@ -1,11 +1,4 @@
-if (typeof web3 !== 'undefined') {
-	web3 = new Web3(web3.currentProvider);
-} else {
-    // set the provider you want from Web3.providers
-    //web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-	web3 = new Web3(new Web3.providers.HttpProvider("http://172.18.64.91:8545"));
-}
-
+//datos del contrato
 var address = '0x2a3d523c56c8848674fe96bad2a2ebd3f20aeee2';
 var EtherProofAPI = [
 	{
@@ -80,6 +73,22 @@ var EtherProofAPI = [
 		"type": "function"
 	}
 ];
+
+var geth_url = "http://172.18.64.91:8545";
+
+var user_address = "0x21DD55B34FE05eB1B396Ef61ea207dD089C05E4e";
+var user_password = "YOUR PASSWORD";
+var transaction_wei_cost = 100000;
+var transaction_gas = 4700000;
+
+
+if (typeof web3 !== 'undefined') {
+	web3 = new Web3(web3.currentProvider);
+} else {
+    // set the provider you want from Web3.providers
+	//web3 = new Web3(new Web3.providers.HttpProvider("http://172.18.64.91:8545"));
+	web3 = new Web3(new Web3.providers.HttpProvider(geth_url));
+}
 var EtherProof = web3.eth.contract(EtherProofAPI).at(address);
 
 function existence() {
@@ -128,4 +137,48 @@ function existence() {
 			}
 		});
 	
+}
+
+function addIndex() {
+	console.log('addIndex...');
+	
+	var hash = document.getElementById('hash_new').value;
+	var existence = document.getElementById('added');	
+	existence.innerHTML = '<h2> Checking ... </h2>';
+	
+	var fileExistence = EtherProof.checkExistence(hash, function(error, result) {			
+		if (error) {
+				console.error(error);
+		} else {
+			console.log(result);
+			if (result) {
+				existence.innerHTML = '<h2> This index does exist already</h2>';
+			} else {
+				//desbloquea la cuenta
+				web3.personal.unlockAccount(user_address, user_password, 15000);
+				
+				//invoca la transaccion
+				EtherProof.addFile.sendTransaction(
+                hash,
+                {
+                        from: user_address, 
+                        value: transaction_wei_cost, //wei
+                        gas: transaction_gas
+                }, function(error, result) {
+					if (error) {
+							console.error(error);
+							existence.innerHTML = '<p> Error: ' + error + '</p>';
+					} else {
+						console.log(result);
+						existence.innerHTML = '<h2> Added: ' + result + '</h2>';
+						
+						var transaction = web3.eth.getTransaction(result);
+						console.log(transaction);
+						
+						existence.innerHTML += '<pre>' + JSON.stringify(transaction) + '</pre>';
+					}
+				});
+			}				
+		}
+	});	
 }
