48a62aafd94fbfbe55f1d37759eaf681e3fc6f69
jmdmpolo <jmdmpolo@users.noreply.github.com>
2018-02-13 10:12:28 +0100

    contract

--- EtherQuerecon/FileRegister.sol
+++ EtherQuerecon/FileRegister.sol
@@ -1,51 +1,51 @@
 pragma solidity ^0.4.18;
 
-contract FileRegister{
+contract FileRegister {
     
-address private creator; 
-
-//constructor
-function EntityContract() public 
-{
-    creator = msg.sender; 
-}
-
-//Standard kill() function to recover funds 
-function kill() public
-{ 
-    if (msg.sender == creator)
-        selfdestruct(creator);  // kills this contract and sends remaining funds back to creator
-}
-
-//struct for Fichero
-struct Fichero {
-	address userAddress;    // usuario que registra el fichero
-    uint fecha;             // fecha cuando se registra el fichero
-}
-
-//internal arrays
-mapping (bytes32 => Fichero ) private ficheros ; // array de ficheros, se utiliza el hash como index
-
-//crea un fichero
-function createFichero(bytes32 ficheroHash) public returns (bool) 
-{
-    //comprueba que no exista ya
-    require(ficheros[ficheroHash].fecha == 0);
-	
-    ficheros[ficheroHash].userAddress = msg.sender; //usuario que lanza la transaccion
-    ficheros[ficheroHash].fecha = now;
+    address private creator;
     
-    return true;	        
-}
-
-//recupera un fichero
-function getFichero(bytes32 ficheroHash) public view returns (address, uint256) {
-
-    //si no existe el fichero da error
-    require(ficheros[ficheroHash].fecha != 0);
+    //constructor
+    function FileRegister() public {
+        creator = msg.sender; 
+    }
     
-    //si existe devuelve sus datos
-    return (ficheros[ficheroHash].userAddress, ficheros[ficheroHash].fecha);
-}
+    //Standard kill() function to recover funds 
+    function kill() public { 
+        if (msg.sender == creator)
+            selfdestruct(creator);  // kills this contract and sends remaining funds back to creator
+    }
+    
+    //struct for Fichero
+    struct Fichero {
+               address userAddress;    // usuario que registra el fichero
+        uint timestamp;         // fecha cuando se registra el fichero
+        uint blockNumber;       // numero de bloque de la transaccion
+    }
+    
+    //internal arrays
+    mapping (bytes32 => Fichero ) private ficheros ; // array de ficheros, se utiliza el hash como index
+    
+    //event
+    event ficheroInfo(
+               bytes32 ficheroHash
+    );
+    
+    //crea un fichero
+    function createFichero(bytes32 ficheroHash) public {
+        
+        require(ficheros[ficheroHash].timestamp == 0); //si no existe el fichero da error
+        
+        ficheros[ficheroHash] = Fichero( msg.sender, now, block.number);
+               ficheroInfo(ficheroHash);
+    }
+    
+    //recupera un fichero
+    function getFichero(bytes32 ficheroHash) public view returns (address, uint, uint) {
+        
+        require(ficheros[ficheroHash].timestamp != 0); //si no existe el fichero da error
+        
+        return (ficheros[ficheroHash].userAddress, ficheros[ficheroHash].timestamp, ficheros[ficheroHash].blockNumber);
+    }
 
 } //end contract
+
