a75429196d0e4a98239cb3748043157a36e2c8fb
jmdmpolo <jmdmpolo@users.noreply.github.com>
2019-04-01 13:01:39 +0200

    Update test.js

--- nodejs/test.js
+++ nodejs/test.js
@@ -3,9 +3,7 @@
 var Web3 = require('web3');
 
 // Show Web3 where it needs to look for a connection to Ethereum.
-//web3 = new Web3(new Web3.providers.HttpProvider('http://99.80.181.35:22000'));
 web3 = new Web3(new Web3.providers.HttpProvider('http://99.80.181.35/rpc'));
-//web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/328aa62401014388a80ff5b030ae69ca'));
 
 // Pintamos info del bloque actual
 console.log('getBlock');
