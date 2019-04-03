8856c8a3853b141373f06cb4f8b426e38a072bc5
jmdmpolo <jmdmpolo@gmail.com>
2019-04-03 15:31:23 +0200

    update

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,18 +1,40 @@
-
+//https://web3js.readthedocs.io/en/1.0/
 // Require the web3 node module.
 var Web3 = require('web3');
 
-// Show Web3 where it needs to look for a connection to Ethereum.
-web3 = new Web3(new Web3.providers.HttpProvider('http://99.80.181.35/rpc'));
+// URL Nodo Alastria
+web3 = new Web3(new Web3.providers.HttpProvider('http://alastria.dbslabs.es/rpc'));
 
-// Pintamos info del bloque actual
-console.log('getBlock');
-web3.eth.getBlock('latest', false, function (err, result) {
-	console.log('getBlock result:');
-	console.log('number:', result.number);
-	console.log(result);
-	console.log(err);
+console.log('web3 version:', web3.version);
+
+web3.eth.getBlockNumber((err, result) => {
+    if(err) console.error('ERROR', err);
+    console.log('blockNumber:', result);
 });
 
+web3.eth.getProtocolVersion((err, result) => {
+    if(err) console.error('ERROR', err);
+    console.log('Protocol:', result);
+});
+
+web3.eth.net.getId((err, result) => {
+    if(err) console.error('ERROR', err);
+    console.log('Net ID:', result);
+});
+
+web3.eth.getBalance("0x407d73d8a49eeb85d32cf465507dd71d507100c1", (err, result) => {
+    if(err) console.error('ERROR', err);
+    console.log('Balance:', result);
+});
+
+//Alastria no expone personal
+try {
+	web3.personal.unlockAccount(walletAddr, walletPwd, 15000);
+	console.log('wallet unlocked');
+} catch(err) {
+	console.log('Error in web3.personal.unlockAccount:', err);
+}
+
 console.log('End');
+
 
